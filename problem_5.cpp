f0c38b0dbaf88e1f76b34ea1cdb94831786fda0f
210001cs22 <104615720+210001cs22@users.noreply.github.com>
2022-10-03 14:44:36 +0500

    Update problem_5.cpp

--- problem_5.cpp
+++ problem_5.cpp
@@ -2,7 +2,22 @@
 one is greater or equal to the second. If the first is greater, it should print 1; if it is smaller, it
 should print 0*/
 
-//ID:
-//Full Name:
+//ID:210001
+//Full Name:Anvarova Sarvinoz 
 
 //Write your code here
+
+#include <iostream>
+using namespace std;
+int main () {
+    int a{0};
+    int b{0};
+    cin >> a;
+    cin >> b;
+    if(a >= b) {
+        cout << 1 << endl;}
+    else {
+            cout << 0 << endl;
+    }
+    return 0;
+}
