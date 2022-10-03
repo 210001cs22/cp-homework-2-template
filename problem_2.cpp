41451c9cbb4da0ca316ef2bc10edb2d0d7a21667
210001cs22 <104615720+210001cs22@users.noreply.github.com>
2022-10-03 14:20:00 +0500

    Update problem_2.cpp

--- problem_2.cpp
+++ problem_2.cpp
@@ -1,7 +1,26 @@
 /* Write C++ program that takes the radius(float) of the circle from user and calculates diameter,
 circumference and area of it. Print the result of the problem. */
 
-//ID:
-//Full Name:
+//ID:210001
+//Full Name:Anvarova Sarvinoz
 
 //Write your code here
+
+#include <iostream>
+using namespace std;
+int main () {
+    float radius{0};
+    cin >> radius;
+    float diameter{0};
+    diameter = 2 * radius;
+    float circumference{0};
+    circumference = 3.14 * diameter;
+    float area {0};
+    area = 3.14 * radius * radius;
+    cout << "The radius is: " << radius << endl;
+    cout << "The diameter is: " << diameter << endl;
+    cout << "The circumference is: " << circumference << endl;
+    cout << "The area is: " << area << endl;
+    return 0;
+}
+
