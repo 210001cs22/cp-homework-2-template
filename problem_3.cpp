566d75bd6679603a5cccbbe1dba7a137a26ad700
210001cs22 <104615720+210001cs22@users.noreply.github.com>
2022-10-03 14:27:39 +0500

    Update problem_3.cpp

--- problem_3.cpp
+++ problem_3.cpp
@@ -1,7 +1,19 @@
 /*Write C++ program that takes the temperature in Celsius and converts it to Fahrenheit. Print
 the result of the problem */
 
-//ID:
-//Full Name:
+//ID:210001
+//Full Name:Anvarova Sarvinoz 
 
 //Write your code here
+
+#include <iostream>
+using namespace std;
+int main () {
+    float celcius{0};
+    cin >> celcius;
+    float fahrenheit{0};
+    fahrenheit = (celcius * 9/5) + 32;
+    cout << "Celcius: " << celcius << endl;
+    cout << "Fahrenheit: " << fahrenheit << endl;
+    return 0;
+}
